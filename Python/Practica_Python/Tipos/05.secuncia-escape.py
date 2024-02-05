74689a106942f1f467e1b1f3bdd50bc104a8b5a7
romina <romirodrigez13@gmail.com>
2024-02-05 11:11:20 -0300

    Agrego archivos de estudio a Python

---
+++ Python/Practica_Python/Tipos/05.secuncia-escape.py
@@ -0,0 +1,7 @@
+# comentario con simbolo numeral
+# \""
+# \'
+# \\
+# \n
+curso = "Ultimate \nPython\""
+print(curso)
---
+++ Python/Practica_Python/funciones/01-funciones.py
@@ -0,0 +1,10 @@
+def hola(nombre, apellido="Y Hermosa"):
+    print("Hola mundo!")
+    print(f"Bienvenida {nombre} {apellido}")
+
+
+hola("Romina", "Rodriguez")
+hola("Eres muy inteligente")
+
+
+hola(apellido="Rodriguez", nombre="Romina")
